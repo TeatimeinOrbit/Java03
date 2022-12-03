50831ecbe15bd292e2ef918d0f40dd01c64366d7
TeatimeinOrbit <ttt.programmer@gmail.com>
2022-12-03 23:05:43 +0900

    [Add] class: java_A.java

---
+++ java_eclipse_test/java_A.java
@@ -0,0 +1,16 @@
+package java_eclipse_test;
+
+public class java_A { 
+	
+	public static void main(String[] args) {
+		String name1;
+		name1 = "Adam";
+		final int NOMBER = 7;
+		System.out.println(name1);
+		System.out.println(NOMBER);
+		
+		name1 = "Boby";
+		System.out.println(name1);
+	}
+	
+}
