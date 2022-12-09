81ee79522d6c7436cc8df8b0f051d15376307538
TeatimeinOrbit <ttt.programmer@gmail.com>
2022-12-09 23:45:24 +0900

    [Add] array01.java

---
+++ java_eclipse_test/array01.java
@@ -0,0 +1,16 @@
+package java_eclipse_test;
+
+public class array01 {
+	public static void main(String[] args) {
+		int[] array1 = new int[3];
+		array1[0] = 60;
+		array1[2] = 20;
+		array1[1] = 20;
+		System.out.println(array1[2]);
+		
+		String[] array2 = new String [] { "一番", "二番", "三番" };
+		System.out.println(array2[2]);
+		
+	}
+
+}
