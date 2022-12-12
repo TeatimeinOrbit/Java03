ac72a38733e3f1da21ff9786f850f67a2d8cfe9c
TeatimeinOrbit <ttt.programmer@gmail.com>
2022-12-12 23:57:05 +0900

    [Add] calculation.java

---
+++ java_eclipse_test/calculation.java
@@ -0,0 +1,18 @@
+package java_eclipse_test;
+
+public class calculation {
+	public static void main(String[] args) {
+		int a = 15;
+		int c;
+		c = a + 3;
+		System.out.println(c);
+		c = a - 2;
+		System.out.println(c);
+		c = a * 5;
+		System.out.println(c);
+		c = a / 3;
+		System.out.println(c);
+		c = a % 4;
+		System.out.println(c);
+	}
+}
