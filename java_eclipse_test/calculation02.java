07cdd9b1f20bd47ac2df764f98a42babd6dc76e6
TeatimeinOrbit <ttt.programmer@gmail.com>
2022-12-15 23:47:53 +0900

    [Add]culculation02.java

---
+++ java_eclipse_test/calculation02.java
@@ -0,0 +1,27 @@
+package java_eclipse_test;
+
+public class calculation02 {
+	public static void main(String[] args) {
+		int a = 10;
+		a++;
+		System.out.println(a);
+		int b = 10;
+		b --;
+		System.out.println(b);
+		
+		System.out.println("Next");
+		
+		int c = 10;
+		c += 2;
+		System.out.println(c);
+		c -= 2;
+		System.out.println(c);
+		c *= 2;
+		System.out.println(c);
+		c /= 2;
+		System.out.println(c);
+		c %= 2;
+		System.out.println(c);
+	}
+
+}
