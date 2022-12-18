4872a0ce9fe2099b2ac7a74722bfa2c384ce0055
TeatimeinOrbit <ttt.programmer@gmail.com>
2022-12-18 23:52:03 +0900

    [Add] hutogo.java

---
+++ java_eclipse_test/hutogo.java
@@ -0,0 +1,20 @@
+package java_eclipse_test;
+
+public class hutogo {
+	public static void main(String[] args) {
+		int a = 5;
+		boolean c;
+		c = a < 5;
+		System.out.println(c);
+		c = a <= 5;
+		System.out.println(c);
+		c = a > 4;
+		System.out.println(c);
+		c = a >= 4;
+		System.out.println(c);
+		c = a == 5;
+		System.out.println(c);
+		c = a != 5;
+		System.out.println(c);
+	}
+}
